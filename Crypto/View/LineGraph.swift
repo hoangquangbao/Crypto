4e002511f2bf02aee554b72c4433ff816961b0b6
Quang Bao <hoangquangbao.developer@gmail.com>
2022-04-19 15:46:52 +0700

    Link Graph - Fix Frame and Add Index Data for Drag Indicator

--- Crypto/View/LineGraph.swift
+++ Crypto/View/LineGraph.swift
@@ -15,34 +15,37 @@ struct LineGraph: View {
     // Offset...
     @State var offset: CGSize = .zero
     
+    @State var showPlot = false
+    @State var translation: CGFloat = 0
+    
     var body: some View {
         
         GeometryReader { proxy in
+            let height = proxy.size.height
+            // Chiều rộng = kích thước ngang / (số phần tử - 1)
+            let width = (proxy.size.width) / CGFloat(data.count - 1)
+            
+            let maxPoint = (data.max() ?? 0) + 100
+            
+            let points = data.enumerated().compactMap { item -> CGPoint in
+                
+                // getting progress and multiplyinh with height...
+                // ???
+                let progress = item.element / maxPoint
+                let pathHeight = progress * height
+                
+                // width...
+                let pathWidth = width * CGFloat(item.offset)
+                
+                //                    return CGPoint(x: pathWidth, y: pathHeight)
+                
+                // Since we need peak to top not bottom...
+                // y = Giá trị càng lớn thì nó sẽ nằm phía dưới vì đó là cách hiển thị của màn hình
+                // Nên ta sẽ đảo ngược sơ đồ lại và + thêm height để dịch chuyển biểu đồ xún
+                return CGPoint(x: pathWidth, y: -pathHeight + height)
+            }
+            
             ZStack {
-                
-                let height = proxy.size.height
-                // Chiều rộng = kích thước ngang / (số phần tử - 1)
-                let width = (proxy.size.width) / CGFloat(data.count - 1)
-                
-                let maxPoint = (data.max() ?? 0) + 100
-                
-                let points = data.enumerated().compactMap { item -> CGPoint in
-                    
-                    // getting progress and multiplyinh with height...
-                    // ???
-                    let progress = item.element / maxPoint
-                    let pathHeight = progress * height
-                    
-                    // width...
-                    let pathWidth = width * CGFloat(item.offset)
-                    
-                    //                    return CGPoint(x: pathWidth, y: pathHeight)
-                    
-                    // Since we need peak to top not bottom...
-                    // y = Giá trị càng lớn thì nó sẽ nằm phía dưới vì đó là cách hiển thị của màn hình
-                    // Nên ta sẽ đảo ngược sơ đồ lại và + thêm height để dịch chuyển biểu đồ xún
-                    return CGPoint(x: pathWidth, y: -pathHeight + height)
-                }
                 // Converting plot as points...
                 
                 // Path....
@@ -88,6 +91,9 @@ struct LineGraph: View {
                         .padding(.vertical,6)
                         .padding(.horizontal,10)
                         .background(Color("Gradient1"),in: Capsule())
+                    // Ta dời vị trí hiển thị của chỉ số đầu và cuối để ko bị che mất
+                        .offset(x: translation < 10 ? 30 : 0)
+                        .offset(x: translation > (proxy.size.width - 60) ? -30 : 0)
                     
                     Rectangle()
                         .fill(Color("Gradient1"))
@@ -107,6 +113,34 @@ struct LineGraph: View {
                         .fill(Color("Gradient1"))
                         .frame(width: 1, height: 55)
                 }
+                // Fixed Frame...
+                // For Gesture Calculation...
+                    .frame(width: 80, height: 170)
+                // 170 / 2 = 85 - 15 => circle ring size...
+                    .offset(y: 70)
+                    .offset(offset)
+                // Show Drag Indicator only has drag gesture
+                    .opacity(showPlot ? 1 : 0)
+                , alignment: .bottomLeading
+            )
+            .contentShape(Rectangle())
+            .gesture(DragGesture()
+                .onChanged({ value in
+                withAnimation { showPlot = true }
+                // Mỗi lần kéo nó sẽ auto giật tới 1 khoảng.
+                    // Trừ bớt 50 là ok
+                    // Trừ 100 thì nó giật lui
+                let translation = value.location.x
+                    
+                // Getting index...
+                    let index = max(min(Int((translation / width).rounded() + 1), data.count - 1), 0)
+                currentPlot = "$ \(data[index])"
+                    self.translation = translation
+                // removing half width...
+                offset = CGSize(width: points[index].x - 40, height: points[index].y - height) })
+                     
+                .onEnded({ value in
+                withAnimation { showPlot = false }})
             )
         }
         .padding(.horizontal, 10)
